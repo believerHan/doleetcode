17c02dd8cb55e1e9f80683831df46babc6567634
SN99914 <hanlf@scc.com.cn>
2025-02-14 11:47:43 +0800

    20250214

--- src/leetcode/y2025/m02/d13/Solution1742.java
+++ src/leetcode/y2025/m02/d13/Solution1742.java
@@ -1,7 +1,22 @@
 package leetcode.y2025.m02.d13;
 
+import java.util.Arrays;
+
 class Solution1742 {
-    public int countBalls(int lowLimit, int highLimit) {
-        
+    public static void main(String[] args) {
+        System.out.println(countBalls(1,1000));
+    }
+    public static int countBalls(int lowLimit, int highLimit) {
+        int[] s = new int[54];
+        for (int i = lowLimit; i <= highLimit; i++) {
+            int n = 0,cur = i;
+            while (cur > 0) {
+                int m = cur % 10;
+                n += m;
+                cur = (cur - m) / 10;
+            }
+            s[n]++;
+        }
+        return Arrays.stream(s).max().getAsInt();
     }
 }
