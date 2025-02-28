ddaf21e22cb3547497389362c7833566fbaa65b6
SN99914 <hanlf@scc.com.cn>
2025-02-28 16:38:05 +0800

    20250228

---
+++ src/leetcode/y2025/m02/d27/TextEditor.java
@@ -0,0 +1,56 @@
+package leetcode.y2025.m02.d27;
+
+//2296
+class TextEditor {
+
+    private StringBuilder sb;
+
+    private int index = 0;
+
+    public TextEditor() {
+        sb = new StringBuilder();
+    }
+
+    public void addText(String text) {
+        sb.insert(index, text);
+        index += text.length();
+    }
+
+    public int deleteText(int k) {
+        int start =index;
+        int deleted = index < k ? 0 : index - k;
+        sb.delete(deleted, index);
+        index = deleted;
+        return start < k ? start : k;
+    }
+
+    public String cursorLeft(int k) {
+        index = index < k ? 0 : index - k;
+        if (index <= 10)
+            return sb.substring(0, index);
+        return sb.substring(index - 10, index);
+    }
+
+    public String cursorRight(int k) {
+        index = (k + index) > sb.length() ? sb.length() : index + k;
+        if (index <= 10)
+            return sb.substring(0, index);
+        return sb.substring(index - 10, index);
+    }
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("123456");
+        //  sb.insert(2,"000");
+        sb.delete(2, 3);
+        System.out.println(sb);
+    }
+}
+
+/**
+ * Your TextEditor object will be instantiated and called as such:
+ * TextEditor obj = new TextEditor();
+ * obj.addText(text);
+ * int param_2 = obj.deleteText(k);
+ * String param_3 = obj.cursorLeft(k);
+ * String param_4 = obj.cursorRight(k);
+ */
---
+++ src/leetcode/y2025/m02/d28/FoodRatings.java
@@ -0,0 +1,23 @@
+package leetcode.y2025.m02.d28;
+//2353 https://leetcode.cn/problems/design-a-food-rating-system/
+class FoodRatings {
+
+    public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+        
+    }
+    
+    public void changeRating(String food, int newRating) {
+        
+    }
+    
+    public String highestRated(String cuisine) {
+        
+    }
+}
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings obj = new FoodRatings(foods, cuisines, ratings);
+ * obj.changeRating(food,newRating);
+ * String param_2 = obj.highestRated(cuisine);
+ */
